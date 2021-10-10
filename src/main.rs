424fdfa91e817286e44e926a344ca8dcc1075980
Konstantin Senekvich <konstantin.senkevich@atomicus.de>
2021-10-11 00:08:17 +0300

    Simple collisions.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use bevy::window::Windows;
+use bevy::sprite::collide_aabb::{Collision, collide};
 use bevy::ecs::system::{
     IntoSystem,
     Query,
@@ -7,6 +9,7 @@ use bevy::ecs::system::{
 };
 use bevy::input::{Input, keyboard::KeyCode};
 use bevy::sprite::ColorMaterial;
+use bevy::render::color::Color;
 use bevy::asset::{Assets, AssetServer};
 use bevy::core::Time;
 use bevy::math::{Vec2, Vec3};
@@ -22,14 +25,17 @@ struct HeroBundle {
     position: Vec3,
 }
 
-struct Car;
+#[derive(Default)]
+struct Car {
+    crashed: bool
+}
 
 struct Health(usize);
 
 struct Name(String);
 
 enum Collider {
-    Floor
+    Wall
 }
 
 fn main() {
@@ -38,47 +44,95 @@ fn main() {
         .add_startup_system(startup.system())
         .add_system(greet_heroes.system())
         .add_system(keyboard_control.system())
+        .add_system(car_collision_system.system())
         .run()
 }
 
 fn keyboard_control(
     input: Res<Input<KeyCode>>,
-    mut cars: Query<(&Car, &mut Transform)>
+    window: Res<Windows>,
+    time: Res<Time>,
+    mut cars: Query<(&Car, &mut Transform)>,
 ) {
-    let shift = 10f32;
+    if input.pressed(KeyCode::R) {
+        let shift_step = 30f32;
+        let mut shift = Vec3::ZERO;
+        for (_car, mut transform) in cars.iter_mut() {
+            transform.translation = shift;
+            shift.y += shift_step;
+        }
+        return;
+    }
+    let primary_window = window.get_primary().unwrap();
+    let height = primary_window.height() / 10f32;
+    let width = primary_window.width() / 10f32;
+    let vertical_speed = height * time.delta_seconds(); 
+    let horizontal_speed = width * time.delta_seconds();
     let mut position_shift = Vec3::ZERO;
     if input.pressed(KeyCode::Up) {
-        position_shift.y += shift;
+        position_shift.y += vertical_speed;
     }
     if input.pressed(KeyCode::Down) {
-        position_shift.y -= shift;
+        position_shift.y -= vertical_speed;
     }
     if input.pressed(KeyCode::Right) {
-        position_shift.x += shift;
+        position_shift.x += horizontal_speed;
     }
     if input.pressed(KeyCode::Left) {
-        position_shift.x -= shift;
+        position_shift.x -= horizontal_speed;
     }
-    for mut transform in cars.iter_mut() {
-        transform.1.translation += position_shift;
+    for (car, mut transform) in cars.iter_mut() {
+        if car.crashed {
+            transform.translation = Vec3::ZERO;
+        } else {
+            transform.translation += position_shift;
+        }
+    }
+}
+
+fn car_collision_system(
+    mut cars: Query<(&mut Car, &Transform)>,
+    colliders: Query<(&Collider, &Transform)>,
+) {
+    let car_size = Vec2::new(50f32, 30f32);
+    for (mut car, car_transform) in cars.iter_mut() {
+        let mut collision_happened = false;
+        for (_collider, collider_transform) in colliders.iter() {
+            let collision = collide(
+                car_transform.translation,
+                car_size,
+                collider_transform.translation,
+                collider_transform.scale.truncate() * Vec2::ONE);
+            if let Some(collision) = collision {
+                println!("Collision {:?}", collision);
+                collision_happened = true;
+                break;
+            } 
+        }
+        car.crashed = collision_happened;
     }
 }
 
 fn startup(
     mut commands: Commands,
     assets_server: Res<AssetServer>,
+    windows: Res<Windows>,
     mut materials: ResMut<Assets<ColorMaterial>>
 ) {
     let blue_car_handle = assets_server.load("blue_car.png");
     let green_car_handle = assets_server.load("green_car.png");
     let red_car_handle = assets_server.load("red_car.png");
 
+    let window = windows.get_primary().unwrap();
+    let width = window.width();
+    let height = window.height();
+
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
     commands
         .spawn_bundle(SpriteBundle {
             material: materials.add(blue_car_handle.into()),
             ..Default::default()
-        }).insert(Car);
+        }).insert(Car::default());
     commands
         .spawn_bundle(SpriteBundle {
             material: materials.add(green_car_handle.into()),
@@ -87,7 +141,7 @@ fn startup(
                 ..Default::default()
             },
             ..Default::default()
-        }).insert(Car);
+        }).insert(Car::default());
     commands
         .spawn_bundle(SpriteBundle {
             material: materials.add(red_car_handle.into()),
@@ -96,7 +150,60 @@ fn startup(
                 ..Default::default()
             },
             ..Default::default()
-        }).insert(Car);
+        }).insert(Car::default());
+
+    commands.spawn_bundle(SpriteBundle {
+        material: materials.add(Color::rgb(0.2, 1.0, 0.2).into()),
+        sprite: Sprite {
+            size: Vec2::new(1f32, 1f32),
+            ..Default::default()
+        },
+        transform: Transform {
+            translation: Vec3::new(-width / 2f32, 0f32, 0f32),
+            scale: Vec3::new(10f32, height, 0f32),
+            ..Default::default()
+        },
+        ..Default::default()
+    }).insert(Collider::Wall);
+    commands.spawn_bundle(SpriteBundle {
+        material: materials.add(Color::rgb(0.2, 1.0, 0.2).into()),
+        sprite: Sprite {
+            size: Vec2::new(1f32, 1f32),
+            ..Default::default()
+        },
+        transform: Transform {
+            translation: Vec3::new(width / 2f32, 0f32, 0f32),
+            scale: Vec3::new(10f32, height, 0f32),
+            ..Default::default()
+        },
+        ..Default::default()
+    }).insert(Collider::Wall);
+    commands.spawn_bundle(SpriteBundle {
+        material: materials.add(Color::rgb(0.2, 1.0, 0.2).into()),
+        sprite: Sprite {
+            size: Vec2::new(1f32, 1f32),
+            ..Default::default()
+        },
+        transform: Transform {
+            translation: Vec3::new(0f32, -height / 2f32, 0f32),
+            scale: Vec3::new(width, 10f32, 0f32),
+            ..Default::default()
+        },
+        ..Default::default()
+    }).insert(Collider::Wall);
+    commands.spawn_bundle(SpriteBundle {
+        material: materials.add(Color::rgb(0.2, 1.0, 0.2).into()),
+        sprite: Sprite {
+            size: Vec2::new(1f32, 1f32),
+            ..Default::default()
+        },
+        transform: Transform {
+            translation: Vec3::new(0f32, height / 2f32, 0f32),
+            scale: Vec3::new(width, 10f32, 0f32),
+            ..Default::default()
+        },
+        ..Default::default()
+    }).insert(Collider::Wall);
 }
 
 fn greet_heroes(time: Res<Time>, query: Query<&Name>) {
